aded7918e811ac1c55984043bae01482aab47834
Joy Wanjiru <wanjirusimiyu@gmail.com>
2024-03-31 07:54:08 +0300

    modify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+
+  form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+
+      const taskDescriptionInput = document.getElementById('new-task-description');
+      const taskDescription = taskDescriptionInput.value.trim();
+
+
+      const listItem = document.createElement('li');
+      listItem.textContent = taskDescription;
+
+
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = '❌';
+      deleteButton.addEventListener('click', function() {
+          listItem.remove();
+      });
+
+      listItem.appendChild(deleteButton);
+
+      taskList.appendChild(listItem);
+
+      taskDescriptionInput.value = '';
+
+    });
+  });
